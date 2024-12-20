83385757be2e81639c38fbc4972cfcbaa7c4d3ec
Matt Edwards <matthew.edwards@wagglebee.net>
2024-12-20 17:01:28 -0500

    Trying Parlot to build the expression grammer

---
+++ src/Hyperbee.ExpressionScript/Parser/ExpressionScriptParser.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Parlot.Fluent;
+
+using static System.Linq.Expressions.Expression;
+using static Parlot.Fluent.Parsers;
+
+namespace Hyperbee.ExpressionScript.Parser;
+
+public class ExpressionScriptParser
+{
+    public static readonly Parser<BinaryExpression> Script;
+
+    static ExpressionScriptParser()
+    {
+        var Identifier = Terms.Identifier();
+        var IntegerLiteral = Terms.Integer();
+
+        // Primary expressions
+        var Primary = Identifier.Then( name => Parameter( typeof( long ), name.ToString() ) );
+
+        // Variable declarations
+        var VariableDeclaration = Terms.Text( "let" ).SkipAnd( Identifier )
+            .AndSkip( Terms.Text( "=" ) )
+            .And( IntegerLiteral ).Then( parts =>
+            {
+
+                var variable = Parameter( typeof( long ), parts.Item1.ToString() );
+                return Assign( variable, Constant( parts.Item2 ) );
+            } );
+
+        Script = VariableDeclaration;
+    }
+
+    public static Expression Parse( string script )
+    {
+        if( Script.TryParse( script, out var result ) )
+        {
+            return result;
+        }
+        return null;
+    }
+
+}
---
+++ test/Hyperbee.ExpressionScript.Tests/ExpressionScriptParserTests.cs
@@ -0,0 +1,23 @@
+﻿using Hyperbee.ExpressionScript.Parser;
+
+using static System.Linq.Expressions.Expression;
+
+namespace Hyperbee.ExpressionScript.Tests;
+
+[TestClass]
+public class ExpressionScriptParserTests
+{
+    [TestMethod]
+    public void Compile_ShouldSucceed_WithScript()
+    {
+        var expression = ExpressionScriptParser.Parse( "let x = 10" );
+
+        var compiled = Lambda( expression );
+
+        var r = compiled.Compile();
+        
+        Assert.IsNotNull( r );
+
+        Assert.IsTrue( true );
+    }
+}
