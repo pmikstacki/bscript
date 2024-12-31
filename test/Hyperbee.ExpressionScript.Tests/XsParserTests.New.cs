3fccd80cbef059528f19df9de98f5a32119b90c4
Matt Edwards <matthew.edwards@wagglebee.net>
2024-12-31 17:20:02 -0500

    Trying to get property of new

--- test/Hyperbee.ExpressionScript.Tests/XsParserTests.New.cs
+++ test/Hyperbee.ExpressionScript.Tests/XsParserTests.New.cs
@@ -20,4 +20,22 @@ public class XsParserNewExpressionTests
         Assert.IsNotNull( result );
         Assert.AreEqual( 42, result.Value );
     }
+
+    [TestMethod]
+    public void Compile_ShouldSucceed_WithNewAndProperty()
+    {
+        var parser = new XsParser { References = [Assembly.GetExecutingAssembly()] };
+
+        var expression = parser.Parse(
+            """
+            new Hyperbee.XS.Tests.SimpleClass(42).Value;
+            """ );
+
+        var lambda = Expression.Lambda<Func<int>>( expression );
+
+        var compiled = lambda.Compile();
+        var result = compiled();
+
+        Assert.AreEqual( 42, result );
+    }
 }
