e8660b7d5292b076ad68fbffc4c49d9f1ba02c24
Brenton Farmer <brent.farmer@wagglebee.net>
2025-01-05 06:49:50 -0800

    move declaration, assignment, new to Values partial class

--- src/Hyperbee.XS/XsParser.cs
+++ src/Hyperbee.XS/XsParser.cs
@@ -124,8 +124,6 @@ public partial class XsParser
         );
     }
 
-    // Expression Parser
-
     private static Parser<Expression> ExpressionParser( Deferred<Expression> statement )
     {
         var expression = Deferred<Expression>();
@@ -280,128 +278,6 @@ public partial class XsParser
             (Terms.Text( "??" ), Coalesce)
         ).Named( "expression" );
 
-    }
-
-    // Value Parsers
-
-    private static Parser<Expression> AssignmentParser( Parser<Expression> expression )
-    {
-        return Terms.Identifier()
-            .And(
-                SkipWhiteSpace(
-                    Terms.Text( "=" )
-                        .Or( Terms.Text( "+=" ) )
-                        .Or( Terms.Text( "-=" ) )
-                        .Or( Terms.Text( "*=" ) )
-                        .Or( Terms.Text( "/=" ) )
-                        .Or( Terms.Text( "??=" ) )
-                )
-            )
-            .And( expression )
-            .Then<Expression>( static ( ctx, parts ) =>
-                {
-                    var (scope, _) = ctx;
-                    var (ident, op, right) = parts;
-
-                    var left = scope.LookupVariable( ident );
-
-                    return op switch
-                    {
-                        "=" => Assign( left, right ),
-                        "+=" => AddAssign( left, right ),
-                        "-=" => SubtractAssign( left, right ),
-                        "*=" => MultiplyAssign( left, right ),
-                        "/=" => DivideAssign( left, right ),
-                        "??=" => Assign( left, Coalesce( left, right ) ),
-                        _ => throw new InvalidOperationException( $"Unsupported operator: {op}." )
-                    };
-                }
-            );
-    }
-
-    private static Parser<Expression> DeclarationParser( Parser<Expression> expression )
-    {
-        return XsParsers.IfIdentifier( "var",
-            Terms.Identifier()
-            .AndSkip( Terms.Char( '=' ) )
-            .And( expression )
-            .Then<Expression>( static ( ctx, parts ) =>
-                {
-                    var (scope, _) = ctx;
-                    var (ident, right) = parts;
-
-                    var left = ident.ToString()!;
-
-                    var variable = Variable( right.Type, left );
-                    scope.Variables.Add( left, variable );
-
-                    return Assign( variable, right );
-                }
-            )
-        );
-    }
-
-    private static Parser<Expression> NewParser( Parser<Expression> expression )
-    {
-        // TODO: Add optional array initializer
-
-        return XsParsers.IfIdentifier( "new",
-            XsParsers.TypeRuntime()
-            .And(
-                OneOf(
-                    Between(
-                        Terms.Char( '(' ),
-                        Arguments( expression ),
-                        Terms.Char( ')' )
-                    ).Then( static parts => (ConstructorType.Object, parts) ),
-                    Between(
-                        Terms.Char( '[' ),
-                        Separated(
-                            Terms.Char( ',' ),
-                            expression
-                        ),
-                        Terms.Char( ']' )
-                    )
-                    //.And( arrayInitializer ) // TODO: Toggle between bounds and init if exists
-                    .Then( static parts => (ConstructorType.ArrayBounds, parts) )
-                )
-            )
-            .Then<Expression>( static parts =>
-            {
-                var (type, (constructorType, arguments)) = parts;  // TODO: Add initializer
-
-                switch ( constructorType )
-                {
-                    case ConstructorType.ArrayBounds:
-                        if ( arguments.Count == 0 )
-                            throw new InvalidOperationException( "Array bounds initializer requires at least one argument." );
-
-                        return NewArrayBounds( type, arguments );
-
-                    case ConstructorType.ArrayInit:
-                        throw new NotImplementedException( "Array initializer not implemented." );
-
-                    //return NewArrayInit( type, arguments );
-
-                    case ConstructorType.Object:
-                        var constructor = type.GetConstructor( arguments.Select( arg => arg.Type ).ToArray() );
-
-                        if ( constructor == null )
-                            throw new InvalidOperationException( $"No matching constructor found for type {type.Name}." );
-
-                        return New( constructor, arguments );
-
-                    default:
-                        throw new InvalidOperationException( $"Unsupported constructor type: {constructorType}." );
-                }
-            } ) );
-    }
-
-    private enum ConstructorType
-    {
-        Object,
-        ArrayBounds,
-        ArrayInit,
     }
 
     // Helper Parsers
