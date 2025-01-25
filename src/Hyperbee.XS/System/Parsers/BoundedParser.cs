c469d538d4d05faa586a78230a53debaf543350a
Brenton Farmer <brent.farmer@wagglebee.net>
2025-01-25 10:16:19 -0800

    refactor bounded parser to a class

--- src/Hyperbee.XS/System/Parsers/BoundedParser.cs
+++ src/Hyperbee.XS/System/Parsers/BoundedParser.cs
@@ -1,7 +1,56 @@
-﻿using Parlot.Fluent;
+﻿using Parlot;
+using Parlot.Fluent;
 
-using static Parlot.Fluent.Parsers;
 namespace Hyperbee.XS.System.Parsers;
+
+public class BoundedParser<T> : Parser<T>
+{
+    private readonly Func<ParseContext, bool> _condition;
+    private readonly Action<ParseContext> _before;
+    private readonly Parser<T> _parser;
+    private readonly Action<ParseContext> _after;
+
+    public BoundedParser( Func<ParseContext, bool> condition, Action<ParseContext> before, Parser<T> parser, Action<ParseContext> after )
+    {
+        _condition = condition;
+        _before = before;
+        _parser = parser ?? throw new ArgumentNullException( nameof( parser ) );
+        _after = after;
+    }
+
+    public override bool Parse( ParseContext context, ref ParseResult<T> result )
+    {
+        context.EnterParser( this );
+
+        var parseResult = new ParseResult<T>();
+
+        var valid = _condition?.Invoke( context ) ?? true;
+
+        if ( valid )
+        {
+            _before?.Invoke( context );
+
+            var start = context.Scanner.Cursor.Position;
+
+            if ( !_parser.Parse( context, ref parseResult ) )
+            {
+                context.Scanner.Cursor.ResetPosition( start );
+
+                if ( _condition == null ) // if condition was true then always return true (this is what IfParser does)
+                    valid = false;
+            }
+            else
+            {
+                result.Set( parseResult.Start, parseResult.End, parseResult.Value );
+            }
+
+            _after?.Invoke( context );
+        }
+
+        context.ExitParser( this );
+        return valid;
+    }
+}
 
 public static partial class XsParsers
 {
@@ -11,10 +60,7 @@ public static partial class XsParsers
         Parser<T> parser,
         Action<ParseContext> after )
     {
-        return If(
-            condition,
-            Bounded( before, parser, after )
-        ).Named( "bounded-if" );
+        return new BoundedParser<T>( condition, before, parser, after );
     }
 
     public static Parser<T> Bounded<T>(
@@ -22,32 +68,20 @@ public static partial class XsParsers
         Parser<T> parser,
         Action<ParseContext> after )
     {
-        return Between(
-            Always().Then<T>( ( ctx, _ ) =>
-            {
-                before?.Invoke( ctx );
-                return default;
-            } ),
-            parser,
-            Always().Then<T>( ( ctx, _ ) =>
-            {
-                after?.Invoke( ctx );
-                return default;
-            } )
-        ).Named( "bounded" );
+        return new BoundedParser<T>( default, before, parser, after );
     }
 
     public static Parser<T> Bounded<T>(
         Action<ParseContext> before,
         Parser<T> parser )
     {
-        return Bounded( before, parser, null );
+        return new BoundedParser<T>( default, before, parser, default );
     }
 
     public static Parser<T> Bounded<T>(
         Parser<T> parser,
         Action<ParseContext> after )
     {
-        return Bounded( null, parser, after );
+        return new BoundedParser<T>( default, default, parser, after );
     }
 }
